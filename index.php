2b662c53ead25880886088337858c98b3656d620
Mert DEMİR <mertdemir1638@gmail.com>
2019-10-16 10:57:34 +0300

    ilk deneme index dosyasi

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    header('Content-Type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Deneme</title>
+</head>
+<body>
+    <?php
+        echo "Selamlar<br><br>PHP INFO;<br><br>
+        
+            Deneme & Test içerik listesi burada görüntülenecek
+        
+        <br><br>";
+        phpinfo();
+    ?>
+</body>
+</html>
